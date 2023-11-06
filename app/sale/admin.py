9a6a8c288935c2392a21115a0de1811b2af7a86b
Franklin Santos <63262717+FrankYou72@users.noreply.github.com>
2023-11-06 17:51:44 -0300

    admin config + show image

--- app/sale/admin.py
+++ app/sale/admin.py
@@ -1,9 +1,41 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from .models.item import Item
 from .models.buyer import Buyer
 from .models.offer import Offer
 
-admin.site.register(Item)
-admin.site.register(Buyer)
-admin.site.register(Offer)
+
+class OfferInline(admin.TabularInline):
+    model = Offer
+    extra = 0
+
+
+
+class ItemAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        try:
+            return mark_safe(f'<img src="{obj.image.url}" width="100" height="100" />')
+        except ValueError:
+            return None
+
+    list_display = ["name", "type", "start_price", "bought", "image_tag"]
+    image_tag.short_description = 'Image Preview'
+    inlines = [OfferInline]
+
+
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ("buyer", "item", "value", "accepted")
+
+
+
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    inlines = [OfferInline]
+
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Buyer, BuyerAdmin)
+admin.site.register(Offer, OfferAdmin)
