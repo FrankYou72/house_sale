3f2726b1254fea79889d922afec9406ac8267d67
Franklin Santos <63262717+FrankYou72@users.noreply.github.com>
2023-11-20 20:57:59 -0300

    payment

--- app/sale/views/views.py
+++ app/sale/views/views.py
@@ -3,14 +3,16 @@ from django.shortcuts import render
 from ..models.buyer import Buyer
 from ..models.offer import Offer, OfferSerializer
 from ..models.item import Item
+from ..models.payment import Payment
 
 def base_view(request):
     return render(request, "base.html")
 
 
 def list_buyers(request):
-    buyers = Buyer.objects.all().order_by('name')
-    return render(request, "buyer.html", context={"buyers": buyers})
+    buyers = Buyer.objects.all().exclude(offer=None).order_by('name')
+    context = {"buyers": buyers}
+    return render(request, "buyer.html", context=context)
 
 
 def list_offers(request, buyer_id):
@@ -18,21 +20,38 @@ def list_offers(request, buyer_id):
     offers = buyer.offer_set.all()
     accepted_offers = offers.filter(accepted=True)
     total = sum([offer.item.start_price for offer in accepted_offers])
+    payments = buyer.payment_set.all()
+    total_paid = sum([payment.value for payment in payments])
+    total_debt = total - total_paid
+    context = {
+        "buyer": buyer,
+        "offers": offers,
+        "total": total,
+        "total_paid": total_paid,
+        "total_debit": total_debt
+    }
 
-    return render(request, "offer.html", context={"buyer": buyer, "offers": offers, "total": total})
+    return render(request, "offer.html", context=context)
 
 
 def dashboard(request):
     items = Item.objects.all()
+    payments = Payment.objects.all()
     total_expected = sum([item.start_price for item in items])
     total_sold = sum([item.start_price for item in items if item.bought])
-    percentage = round(100*total_sold/total_expected, 2)
+    total_paid = sum([payment.value for payment in payments])
 
-    return render(request, "dashboard.html", context={
+    percentage = round(100*total_sold/total_expected, 2)
+    percentage_paid = round(100*total_paid/total_expected, 2)
+    context = {
         "total_expected": total_expected,
         "total_sold": total_sold,
-        "percentage": percentage
-    })
+        "percentage": percentage,
+        "total_paid": total_paid,
+        "percentage_paid": percentage_paid
+    }
+
+    return render(request, "dashboard.html", context=context)
 
 
 def payment(request, buyer_id):
@@ -43,17 +62,12 @@ def payment(request, buyer_id):
     payments = buyer.payment_set.all()
     total_paid = sum([payment.value for payment in payments])
     total_debt = total_bought - total_paid
+    context = {
+        "buyer": buyer,
+        "payments": payments,
+        "total_bought": total_bought,
+        "total_paid": total_paid,
+        "total_debt": total_debt
+    }
 
-    return render(
-        request,
-        "payment.html",
-        context={
-            "buyer": buyer,
-            "payments": payments,
-            "total_bought": total_bought,
-            "total_paid": total_paid,
-            "total_debt": total_debt
-        })
-
-
-
+    return render(request, "payment.html")
